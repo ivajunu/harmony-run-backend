705170efea4a8694a89e107e766796db67fe62da
ivajunu <ivanna.junuzovic@live.se>
2024-05-30 19:21:57 +0200

    colon

--- routes/users.ts
+++ routes/users.ts
@@ -34,7 +34,7 @@ router.get("/users", async (req, res) => {
   }
 });
 
-router.put("/users:id", async (req, res) => {
+router.put("/users/:id", async (req, res) => {
   try {
     const { id } = req.params;
     const { username, name, email, password } = req.body;
